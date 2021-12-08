d71d92509dd265a4f2322b70ea92e7a156d3fba1
Timothy Legge <timlegge@gmail.com>
2021-12-08 18:39:54 -0400

    Bump version number

--- lib/Dist/Zilla/Plugin/SignReleaseNotes.pm
+++ lib/Dist/Zilla/Plugin/SignReleaseNotes.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::SignReleaseNotes;
 
-our $VERSION = '0.0001';
+our $VERSION = '0.0002';
 
 # ABSTRACT: Create and signs a 'Release' notes file
 use Moose;
