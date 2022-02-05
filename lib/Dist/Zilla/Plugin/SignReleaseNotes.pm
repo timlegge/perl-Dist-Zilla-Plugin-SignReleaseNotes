9000d39b80eb260152b27173c164327547c7de3f
Timothy Legge <timlegge@gmail.com>
2022-02-04 21:41:28 -0400

    Increment version number

--- lib/Dist/Zilla/Plugin/SignReleaseNotes.pm
+++ lib/Dist/Zilla/Plugin/SignReleaseNotes.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::SignReleaseNotes;
 
-our $VERSION = '0.0003';
+our $VERSION = '0.0004';
 
 # ABSTRACT: Create and signs a 'Release' notes file
 use Moose;
